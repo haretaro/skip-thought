9b74f8bcda5f26fa3ef9777dbcabc0a5b7efc2db
haretaro <frontend.1000@gmail.com>
2017-02-09 19:02:30 +0900

    いろいろ関数化した

--- load.py
+++ load.py
@@ -8,51 +8,51 @@ import pickle
 import train
 from train import SkipThought, BPTTUpdater, DocumentIterator
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU ID')
-parser.add_argument('--unit', '-u', type=int, default=100)
-parser.add_argument('--resume', '-r', type=str, default='')
-parser.add_argument('--batchsize', '-b', type=int, default=2)
-parser.add_argument('--bproplen', '-l', type=int, default=35, help='length of trancated BPTT')
-parser.add_argument('--epoch', '-e', type=int, default=100, help='epoch')
-parser.add_argument('--out', type=str, default='result', help='Directory to output the result')
-args = parser.parse_args()
+train.xp = np
+xp = np
 
-xp = cuda.cupy if args.gpu >= 0 else np
-train.xp = xp
 f = open('vocab.bin', 'rb')
 word2index = pickle.load(f)
 f.close()
 index2word = {wid: word for word, wid in word2index.items()}
 n_vocab = len(word2index)
-model = SkipThought(n_vocab, args.unit, word2index['\n'])
-docs_data = docs_to_index(word2index, 'data')
-train_iter = DocumentIterator(docs_data, args.batchsize)
-optimizer = chainer.optimizers.Adam()
-optimizer.setup(model)
-if args.gpu >= 0:
-    chainer.cuda.get_device(args.gpu).use()
-    model.to_gpu()
-updater = BPTTUpdater(train_iter, optimizer, args.bproplen, args.gpu)
-trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
-chainer.serializers.load_npz(args.resume, trainer)
 
-in_data = [word2index[x] for x in ['吾輩', 'は', '猫', 'で', 'ある', '\n']]
-in_data = [word2index[x] for x in ['名前', 'は', 'まだ', 'ない', '\n']]
-print(in_data)
-model.encoder.reset()
-vec = model.encoder(xp.asarray([in_data], dtype=np.int32))
-print(vec.data)
+def load_model(snap_shot, n_vocab, unit=100, batchsize=2, bproplen=35, gpu=-1, out='result', data_directory='data'):
+    docs_data = docs_to_index(word2index, data_directory)
+    model = SkipThought(n_vocab, unit, word2index['\n'])
+    train_iter = DocumentIterator(docs_data, batchsize)
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+    updater = BPTTUpdater(train_iter, optimizer, bproplen, gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=out)
+    chainer.serializers.load_npz(snap_shot, trainer)
+    return model
 
-model.self_decoder.reset()
-out = model.self_decoder(vec, train=False)
-print(out)
-
-out_words = [index2word[x[0]] for x in out]
-print(out_words)
+def to_vector(model, sentence):
+    model.encoder.reset()
+    vec = model.encoder(xp.asarray([in_data], dtype=np.int32))
+    return vec
 
 
-model.next_decoder.reset()
-out = model.next_decoder(vec, train=False)
-out_words = [index2word[x[0]] for x in out]
-print(out_words)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU ID')
+    parser.add_argument('--unit', '-u', type=int, default=100)
+    parser.add_argument('--resume', '-r', type=str, default='')
+    parser.add_argument('--batchsize', '-b', type=int, default=2)
+    parser.add_argument('--bproplen', '-l', type=int, default=35, help='length of trancated BPTT')
+    parser.add_argument('--epoch', '-e', type=int, default=100, help='epoch')
+    parser.add_argument('--out', type=str, default='result', help='Directory to output the result')
+    args = parser.parse_args()
+
+    model = load_model(args.resume, n_vocab, args.unit, args.batchsize, args.bproplen, args.gpu, args.out, 'data')
+
+    in_data = [word2index[x] for x in ['名前', 'は', 'まだ', 'ない', '\n']]
+
+    vec = to_vector(model, in_data)
+    print(vec.data)
+
+    model.prev_decoder.reset()
+    out = model.self_decoder(vec, train=False)
+    out = [index2word[x[0]] for x in out]
+    print(out)
